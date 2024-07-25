39d0608e8e5ef7968b240c64367c8f01b251b97a
Momchil Tsanov <momchil395@gmail.com>
2024-07-25 06:28:22 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include "StacksAndQueues/Stack&Queues.h"
 #include "MapAndSet/Maps&Sets.h"
 
-
+int* findFirstNegativePtr(int numbers[], int length) {
+    for (int i = 0; i < length; i++) {
+        if (numbers[i] < 0) {
+            return &numbers[i];
+        }
+    }
+    return nullptr;
+}
 
 int main() {
 
 
-return 0;
+    int numbers[] {2, 4, -1, 2,5, 1,2, 6, 12};
+    int* negativePtr = findFirstNegativePtr(numbers, 4);
+    if (negativePtr != nullptr) {
+        cout << *negativePtr;
+    }
+    else cout << "no negative numbers" << endl;
+
 }
